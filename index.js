daea30e6160cf199d91372802b9ad59ece7c1b07
JustEddie <skdpwls830@gmail.com>
2022-07-21 17:29:41 +1000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.getElementById("main").remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+const newContent = document.createTextNode("Eddie is the champion");
+newHeader.appendChild(newContent);
+newHeader.setAttribute("id","victory");
